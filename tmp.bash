eead8338600f6a303114c3124c30b81dbb47fa6b
watcher00090 <james00090@gmail.com>
2020-07-07 11:47:22 -0700

    added project

---
+++ tmp.bash
@@ -0,0 +1,6 @@
+echo "# backstage-spring-service-test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/watcher00090/backstage-spring-service-test.git
+git push -u origin master
